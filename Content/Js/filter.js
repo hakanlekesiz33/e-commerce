61042e57ebea4b7b32ec07ffae0f861db4870cdd
Hakan LEKESİZ <hakan.lekesiz@baogrenci.com>
2022-12-17 12:33:06 +0300

    filter

--- Content/Js/filter.js
+++ Content/Js/filter.js
@@ -1,6 +1,7 @@
 var Filter = {
     Apis: {
         categories: "https://dummyjson.com/products/categories",
+        products: "https://dummyjson.com/products",
     },
     Elements: {
         categoryList: document.getElementById("category-list"),
@@ -10,15 +11,80 @@ var Filter = {
     },
     Status: {
         categories: [],
+        products: [],
         query: "",
-        selectedCategories: "",
+        selectedCategories: [],
         sort: "",
     },
     Actions: {
         //Sayfa ilk açıldığında istenilen fonksiyonları çalıştrıacak
         init: () => {
             Filter.Actions.getAllCategories(); //  get all categories
+            Filter.Actions.getAllProducts(); //  get all products
         },
+
+        //search inputuna her yazı yazıldığında bu fonksiyon çalışır
+        handleSearch: (sender) => {
+            Filter.Status.query = sender.value;
+            Filter.Actions.filter();
+        },
+
+        handleCategoryChange: () => {
+            const checkedCategories = document.querySelectorAll("#category-list input:checked");
+            const checkedIds = [];
+            for (let i = 0; i < checkedCategories.length; i++) {
+                const element = checkedCategories[i];
+                checkedIds.push(element.getAttribute("id"));
+            }
+
+            Filter.Status.selectedCategories = checkedIds;
+            Filter.Actions.filter();
+        },
+        //kategorileri saerch value'ya göre gizliyip gösterecek
+        handleCategorySearch: (sender) => {
+            var categoriesLiEl = document.querySelectorAll("#category-list li");
+
+            for (let i = 0; i < categoriesLiEl.length; i++) {
+                const element = categoriesLiEl[i];
+                var categoryName = element.querySelector("label").innerText;
+                if (categoryName.toLocaleLowerCase().includes(sender.value.toLocaleLowerCase())) {
+                    element.style.display = "flex";
+                }
+                else {
+                    element.style.display = "none";
+                    element.querySelector("input").checked = false;
+                }
+
+
+            }
+        },
+
+        //filtreleme fonskiynları
+        filter: () => {
+            let apiUrl = "";
+            if (Filter.Status.query) {
+                apiUrl = Filter.Apis.products + "/search?q=" + Filter.Status.query;
+            }
+            else {
+                apiUrl = Filter.Apis.products;
+            }
+
+            fetch(apiUrl)
+                .then(res => res.json())
+                .then(res => {
+                    var selectedCategories = Filter.Status.selectedCategories;
+
+                    if (selectedCategories.length > 0) {
+                        Filter.Status.products = res.products.filter(x => selectedCategories.includes(x.category))
+                    }
+                    else {
+                        Filter.Status.products = res.products;
+                    }
+
+                    Filter.Actions.appendProductsToHtml();
+                });
+        },
+
         //kategori listesini html'e ekliyor
         appendCategoriesToHtml: () => {
             Filter.Elements.categoryList.innerHTML = "";
@@ -39,6 +105,27 @@ var Filter = {
                 Filter.Elements.categoryList.appendChild(div.querySelector("li"));
             }
         },
+
+        //ürün listesini html e ekliyor
+        appendProductsToHtml: () => {
+            Filter.Elements.productList.innerHTML = "";
+            for (let i = 0; i < Filter.Status.products.length; i++) {
+                const product = Filter.Status.products[i];
+
+                var div = document.createElement("div");
+                div.innerHTML = Filter.Elements.cardTemp.innerHTML;
+
+                div.querySelector("a").setAttribute("href", "/product?id=" + product.id);
+                div.querySelector("img").setAttribute("src", product.thumbnail);
+                div.querySelector("img").setAttribute("alt", product.title);
+                div.querySelector("h5").innerText = product.title;
+                div.querySelector("p").innerText = product.description;
+
+                Filter.Elements.productList.appendChild(div.querySelector("a"));
+
+            }
+        },
+
         //api'den tüm kategori listesini getiriyor
         getAllCategories: () => {
             fetch(Filter.Apis.categories)
@@ -48,6 +135,17 @@ var Filter = {
                     Filter.Actions.appendCategoriesToHtml();
                 });
         },
+
+        //api'den tüm ürün listesini getiriyor
+        getAllProducts: () => {
+            fetch(Filter.Apis.products)
+                .then(res => res.json())
+                .then(res => {
+                    Filter.Status.products = res.products;
+                    Filter.Actions.appendProductsToHtml();
+                });
+        },
+
         //var olan filtreleri temizleyecez
         clear: () => {
 
