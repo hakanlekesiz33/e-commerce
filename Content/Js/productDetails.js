a0b731d799e3248fea396cd5e43853cd30d4f1c7
Hakan LEKESİZ <hakan.lekesiz@baogrenci.com>
2022-12-18 13:57:49 +0300

    search

--- Content/Js/productDetails.js
+++ Content/Js/productDetails.js
@@ -1,8 +1,9 @@
 var Filter = {
     Apis: {
-        products: "https://dummyjson.com/products",
+        products: "https://dummyjson.com/products/",
     },
     Elements: {
+        productTitle: document.getElementById("product-title")
 
     },
 
@@ -14,12 +15,18 @@ var Filter = {
 
         //ürün detaylarını getiren endpoint çalışacak
         getProductDetails: () => {
-            debugger
+        debugger
 
             const urlParams = new URLSearchParams(window.location.search);
             const id = urlParams.get('id');
-            if (id) {
 
+            if (id) {
+                fetch(Filter.Apis.products + id)
+                    .then(res => res.json())
+                    .then(res => {
+                        Filter.Elements.productTitle.innerText = res.title;
+
+                    });
             }
             else {
                 alert("hatalı url")
