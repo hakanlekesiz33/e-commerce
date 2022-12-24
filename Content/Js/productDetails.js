3e2f0e41645d3b01f8365fd5c444ce7d417f1861
Hakan LEKESİZ <hakan.lekesiz@baogrenci.com>
2022-12-24 13:59:45 +0300

    ürün detay

--- Content/Js/productDetails.js
+++ Content/Js/productDetails.js
@@ -3,8 +3,12 @@ var Filter = {
         products: "https://dummyjson.com/products/",
     },
     Elements: {
-        productTitle: document.getElementById("product-title")
-
+        productTitle: document.getElementById("product-title"),
+        bigImage: document.getElementById("big-image"),
+        smallImagesContainer: document.getElementById("small-images-container"),
+        rating: document.getElementById("rating"),
+        price: document.getElementById("price"),
+        desc: document.getElementById("desc"),
     },
 
     Actions: {
@@ -15,7 +19,6 @@ var Filter = {
 
         //ürün detaylarını getiren endpoint çalışacak
         getProductDetails: () => {
-        debugger
 
             const urlParams = new URLSearchParams(window.location.search);
             const id = urlParams.get('id');
@@ -25,6 +28,27 @@ var Filter = {
                     .then(res => res.json())
                     .then(res => {
                         Filter.Elements.productTitle.innerText = res.title;
+                        Filter.Elements.desc.innerText = res.description;
+                        Filter.Elements.price.innerText = res.price + " TL";
+                        Filter.Elements.rating.innerText = res.rating;
+                        Filter.Elements.bigImage.setAttribute("src", res.images[0]);
+
+                        for (let i = 0; i < res.images.length; i++) {
+                            const imgSrc = res.images[i];
+                            const imgEl = document.createElement("img");
+                            imgEl.setAttribute("src", imgSrc);
+                            imgEl.setAttribute("onclick", "Filter.Actions.changeImage(this)");
+
+                            if (i === 0) {
+                                imgEl.classList.add("active");
+                            }
+
+                            Filter.Elements.smallImagesContainer.appendChild(imgEl);
+
+                            if (i === 2) {
+                                break;
+                            }
+                        }
 
                     });
             }
@@ -33,6 +57,17 @@ var Filter = {
             }
 
 
+        },
+
+        changeImage: (sender) => {
+            const activeEl = Filter.Elements.smallImagesContainer.querySelector(".active");
+            activeEl.classList.remove("active");
+
+            sender.classList.add("active");
+
+            const imgSrc = sender.getAttribute("src");
+            Filter.Elements.bigImage.setAttribute("src", imgSrc);
+ 
         }
 
     },
